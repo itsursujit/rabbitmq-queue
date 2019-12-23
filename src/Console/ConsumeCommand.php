58849c1d2e6842eb4e2d0c5a2c45093b27c87d4d
sujit <itsursujit@gmail.com>
2019-12-23 11:20:28 +0545

    Initial Comment

--- src/Console/ConsumeCommand.php
+++ src/Console/ConsumeCommand.php
@@ -15,7 +15,7 @@ class ConsumeCommand extends WorkCommand
                             {--stop-when-empty : Stop when the queue is empty}
                             {--delay=0 : The number of seconds to delay failed jobs}
                             {--force : Force the worker to run even in maintenance mode}
-                            {--memory=128 : The memory limit in megabytes}
+                            {--memory=512 : The memory limit in megabytes}
                             {--sleep=3 : Number of seconds to sleep when no job is available}
                             {--timeout=6000 : The number of seconds a child process can run}
                             {--tries=1 : Number of times to attempt a job before logging it failed}
