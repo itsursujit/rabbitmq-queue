b284b7bf933c67591e1cadf2f45f418278094868
Sujit <itsursujit@gmail.com>
2020-03-09 18:41:20 +0545

    Removing custom formatting of Job output

--- src/Console/ConsumeCommand.php
+++ src/Console/ConsumeCommand.php
@@ -62,37 +62,4 @@ class ConsumeCommand extends WorkCommand
 
         return '';
     }
-
-    /**
-     * Format the status output for the queue worker.
-     *
-     * @param  \Illuminate\Contracts\Queue\Job  $job
-     * @param  string  $status
-     * @param  string  $type
-     * @return void
-     */
-    protected function writeStatus($job, $status, $type)
-    {
-        try
-        {
-            $command = unserialize($job->payload()['data']['command']);
-            $jobName = str_replace('App\\Jobs\\', '', $job->resolveName());
-            $tags = [
-                'Job' => $jobName,
-                'Status' => $status
-            ];
-            if (property_exists($command, 'tags'))
-            {
-                $tags = array_merge($tags, $command->tags);
-            } else
-            {
-                $tags['JobId'] = $job->getJobId();
-            }
-            $output = json_encode($tags);
-            $this->output->writeln(sprintf("<{$type}>%s", $output));
-        } catch (\Throwable $exc)
-        {
-            parent::writeStatus($job, $status, $type);
-        }
-    }
 }
